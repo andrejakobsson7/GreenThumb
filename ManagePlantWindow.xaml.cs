ad49acfaf0397c5e13f390aca09349b39f753b07
andrejakobsson7 <andre.jakobsson7@hotmail.com>
2023-12-04 22:30:58 +0100

    Implement ManagePlantWindow

---
+++ ManagePlantWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using GreenThumb.Models;
+using System.Windows;
+
+namespace GreenThumb
+{
+    /// <summary>
+    /// Interaction logic for ManagePlantWindow.xaml
+    /// </summary>
+    public partial class ManagePlantWindow : Window
+    {
+        public ManagePlantWindow()
+        {
+            InitializeComponent();
+        }
+        public ManagePlantWindow(PlantModel plantToDisplay)
+        {
+            InitializeComponent();
+        }
+    }
+}
